59f9b17e869adedbae5f8f8025934de004aaf16d
joan <yuangmr@gmail.com>
2015-03-04 18:35:15 +0100

    + added some styles + using devise to signup schools

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #define the root page
   root 'static_pages#home'
   get 'myscope' => 'scope#myscope'
+  get 'help'    => 'static_pages#help'
   resources :languages
 
   resources :countries
