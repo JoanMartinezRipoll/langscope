6b52d2644cd9187b53f70bfb2d95c17ca3b4a69d
joan <yuangmr@gmail.com>
2015-03-05 12:48:05 +0100

    added validation for name, address and plz during signup

--- test/integration/school_signup_test.rb
+++ test/integration/school_signup_test.rb
@@ -12,10 +12,13 @@ class SchoolSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template  'schools/registrations/new'
     assert_select 'div#error_explanation' do |element|
-      assert_select "li", 3
+      assert_select "li", 6
       assert_match "Password is too short (minimum is 8 characters)", element.to_s
       assert_match "Password confirmation doesn't match Password", element.to_s
       assert_match "Email is invalid", element.to_s
+      assert_match "Name can't be blank", element.to_s
+      assert_match "Address can't be blank", element.to_s
+      assert_match "Plz can't be blank", element.to_s
     end
   end
 
@@ -26,7 +29,9 @@ class SchoolSignupTest < ActionDispatch::IntegrationTest
       post school_registration_path, school: { name:  "Example school",
                                                email: "school@example.com",
                                                password:              "password",
-                                               password_confirmation: "password" }
+                                               password_confirmation: "password",
+                                               address: "street 4",
+                                               plz: "12324" }
     end
   end
 end
